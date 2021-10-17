bf94c81bdd2728c00b598b6d569c60b40c5e1c40
Valeriu-Cristian Mandeal <mandealvcristian@gmail.com>
2021-10-17 18:37:55 +0300

    Variables

---
+++ learnJS.js
@@ -0,0 +1,16 @@
+var firstNum = 10;
+console.log(firstNum);
+var secondNum = 15;
+console.log(secondNum);
+
+// Rules for naming JS variables
+// - name can contain letters, digits, _ and $
+var mySubject1, math$
+// name cannot start with a digit
+var maths;
+// name can start with an _ or $
+var _subjectmarks, $rankInClass;
+// names are case sensitive
+var x, X;
+// names cannot be reserved keywords
+var varName;
