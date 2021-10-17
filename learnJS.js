40b9b4a898a0c446720cfcdc62b34f34ba85764c
Valeriu-Cristian Mandeal <mandealvcristian@gmail.com>
2021-10-17 18:38:32 +0300

    Numbers

--- learnJS.js
+++ learnJS.js
@@ -1,16 +1,28 @@
-var firstNum = 10;
-console.log(firstNum);
-var secondNum = 15;
-console.log(secondNum);
+console.clear()
+var num1=10; // integer
+var num2=10.9; // decimal number
+console.log(typeof(num1));
+console.log(typeof(num2));
 
-// Rules for naming JS variables
-// - name can contain letters, digits, _ and $
-var mySubject1, math$
-// name cannot start with a digit
-var maths;
-// name can start with an _ or $
-var _subjectmarks, $rankInClass;
-// names are case sensitive
-var x, X;
-// names cannot be reserved keywords
-var varName;
+var num3 = 15;
+var sum = num3 + num1 + num2;
+console.log(sum); // addition
+
+// substraction
+var diff = num3 - num1;
+console.log(diff);
+
+// multiplication
+var mul = num3 * num1;
+console.log(mul);
+
+// division
+var div = num3 / num1;
+
+var divByZero = num3 /0 ;
+console.log(divByZero);
+console.log(typeof(divByZero));
+
+var mulByString = num3 * 'A';
+console.log(mulByString);
+console.log(typeof(mulByString));
