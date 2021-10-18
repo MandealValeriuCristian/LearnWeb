d0110aaf0b3b7dcefb30d0b39c3c812ba2423fea
Valeriu-Cristian Mandeal <mandealvcristian@gmail.com>
2021-10-18 13:11:02 +0300

    Arithmetic Operators

--- learnJS.js
+++ learnJS.js
@@ -1,40 +1,34 @@
-var currentDay = 'Sun'
-if (currentDay == 'Mon')
-console.log('Timmings: 10:00-6:00')
-else if(currentDay == 'Tue')
-console.log('Timmings: 9:00-5:00')
-else if(currentDay == 'Wed')
-console.log('Timmings: 9:30-5:30')
-else if(currentDay == 'Thu')
-console.log('Timmings: 10:15-6:15')
-else if(currentDay == 'Fri')
-console.log('Timmings: 9:05-5:05')
-else if(currentDay == 'Sat')
-console.log('Timmings: 9:00-5:00')
-else if(currentDay == 'Sun')
-console.log('Timmings: 9:00-1:00')
+var num1 = 10;
+var num2 = 15;
+var num3 = 20;
 
-switch(currentDay){
-    case 'Mon':
-        console.log('Timmings: 10:00-6:00')
-        break;
-    case 'Tue':
-        console.log('Timmings: 9:00-5:00')
-        break;
-    case 'Wed':
-        console.log('Timmings: 9:30-5:30')
-        break;
-    case 'Thu':
-        console.log('Timmings: 10:15-6:15')
-        break;
-    case 'Fri':
-        console.log('Timmings: 9:05-5:05')
-        break;
-    case 'Sat':
-        console.log('Timmings: 9:00-5:00')
-        break;
-    case 'Sun':
-        console.log('Timmings: 9:00-1:00')
-        break;
-}
+// Addition
+console.log(num1 + num2)
+console.log(num1 + num2 + num3)
 
+// Substraction
+console.log(num2 - num1)
+console.log(num3 - num2 - num1)
+
+// Multiplication
+console.log(num1*num2)
+
+// Division
+console.log(num2 / num1)
+
+// Modulus
+console.log(num2 % num1)
+
+// Increment - Post and Pre
+console.log("increment")
+console.log(num1++)
+console.log(num1++)
+console.log(num1)
+
+console.log(++num1)
+console.log(num1)
+
+// Decrement - Post and  Pre
+console.log('decrement')
+console.log(--num1)
+console.log(num1)
