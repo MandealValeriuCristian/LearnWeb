047b3af04661303f71bb87f38907ee9f846009cd
Valeriu-Cristian Mandeal <mandealvcristian@gmail.com>
2021-10-18 13:15:47 +0300

    Assignment Operators

--- learnJS.js
+++ learnJS.js
@@ -1,34 +1,27 @@
-var num1 = 10;
-var num2 = 15;
-var num3 = 20;
+var num1 = 15;
+var num2 = 30;
 
-// Addition
-console.log(num1 + num2)
-console.log(num1 + num2 + num3)
+// =
+// num1 = 20;
 
-// Substraction
-console.log(num2 - num1)
-console.log(num3 - num2 - num1)
-
-// Multiplication
-console.log(num1*num2)
-
-// Division
-console.log(num2 / num1)
-
-// Modulus
-console.log(num2 % num1)
-
-// Increment - Post and Pre
-console.log("increment")
-console.log(num1++)
-console.log(num1++)
+// +=
+num1 +=10; // num1 = num1 + 10
+num1 += num2 // num1 = num1 + num2
 console.log(num1)
 
-console.log(++num1)
+// -=
+num1 -=10; // num1 = num1 - 10
+num1 -= num2 // num1 = num1 - num2
 console.log(num1)
 
-// Decrement - Post and  Pre
-console.log('decrement')
-console.log(--num1)
+// *=
+num1 *= 20 // num1 = num1 * 20
+console.log(num1)
+
+// /=
+num1 /= 10 // num1 = num1 /10
+console.log(num1)
+
+// %=
+num1 %= 10 // num1 = num1 % 10
 console.log(num1)
