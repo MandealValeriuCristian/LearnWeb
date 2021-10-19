d60aa625de8d5e555dd8048cc36f2fe98c436a81
Valeriu-Cristian Mandeal <mandealvcristian@gmail.com>
2021-10-19 21:18:08 +0300

    Arrays Part 1

--- learnJS.js
+++ learnJS.js
@@ -1,22 +1,27 @@
-var nAgera = {
-    name: "Agera",
-    manufacturer:{
-        name: "Koenigsegg",
-        location: "Sweden"
-    },
-    topSpeed: 429,
-    color: "Black",
-    spoilers: false,
-    applyBrakes: function(){
-        setTimeout(function(){
-            console.log('Car Stopped')        
-        }, 5000)
-    }
-}
+var Friends = ['Shaw', 'Clara', 'Vab', 'Kun', 'Jacob', 'Dina']
+console.log(Friends)
+console.log(typeof(Friends))
 
-console.log(nAgera.name)
-console.log(nAgera.topSpeed)
-console.log(nAgera.manufacturer)
-console.log(nAgera.manufacturer.name)
-// console.log(nAgera.applyBrakes())
-console.log(nAgera.applyBrakes)
+console.log(Friends[1])
+
+// update a value
+Friends[0] = 'Shawn'
+console.log(Friends)
+
+// add more items
+Friends[6] = 'Alan'
+console.log(Friends)
+
+Friends[10] = 'Tiffany'
+console.log(Friends)
+
+Friends[Friends.length] = 'Rob'
+console.log(Friends)
+
+Friends.push('Jack')
+console.log(Friends)
+
+// delete a value
+var name= Friends.pop()
+console.log(name)
+console.log(Friends)
