cdf27af5327ec45abec0f75d91f4b3f60d279326
Valeriu-Cristian Mandeal <mandealvcristian@gmail.com>
2021-10-19 21:28:16 +0300

    Arrays Part 2

--- learnJS.js
+++ learnJS.js
@@ -1,27 +1,33 @@
 var Friends = ['Shaw', 'Clara', 'Vab', 'Kun', 'Jacob', 'Dina']
-console.log(Friends)
-console.log(typeof(Friends))
-
-console.log(Friends[1])
-
-// update a value
-Friends[0] = 'Shawn'
+Friends.splice(3, 0, 'Donna', 'Rachel')
 console.log(Friends)
 
-// add more items
-Friends[6] = 'Alan'
+// 1st tells where to start
+// 2nd tells how many items to be deleted
+// 3rt and 4th and so on tells what items to be added
 console.log(Friends)
 
-Friends[10] = 'Tiffany'
+Friends.splice(0, 0, 'Harvey')
+
+// delete item at specific position
+Friends.splice(2,1)
 console.log(Friends)
 
-Friends[Friends.length] = 'Rob'
+Friends.splice(0,2)
 console.log(Friends)
 
-Friends.push('Jack')
+// concatenation
+var OfficeFriends = ['Nina', 'Aron', 'Katie', 'Vic', 'Paul']
+var AllFriends = Friends.concat(OfficeFriends)
+console.log('==========================')
 console.log(Friends)
+console.log(OfficeFriends)
+console.log(AllFriends)
 
-// delete a value
-var name= Friends.pop()
-console.log(name)
-console.log(Friends)
+// sorting ascending and descending
+
+AllFriends.sort();
+console.log(AllFriends)
+
+AllFriends.reverse()
+console.log(AllFriends)
