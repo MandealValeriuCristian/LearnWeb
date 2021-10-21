f6d9145105906e3b9faaf5ea694b73c85c08a71b
Valeriu-Cristian Mandeal <mandealvcristian@gmail.com>
2021-10-21 11:36:38 +0300

    Variable Hoisting

--- learnJS.js
+++ learnJS.js
@@ -1,8 +1,4 @@
-var num1 = 10;
-console.log(num1)
-function sum(num2)
-{
-    console.log(num1)
-    console.log(num1 + num2)
-}
-sum(20);
+console.log(num1);
+num1 = 10;
+console.log(num1);
+var num1
