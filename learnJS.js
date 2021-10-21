1f33ba09bb354167b146afa387b1381cb309a828
Valeriu-Cristian Mandeal <mandealvcristian@gmail.com>
2021-10-21 09:51:45 +0300

    Functions

--- learnJS.js
+++ learnJS.js
@@ -1,18 +1,8 @@
-var list = [1, 2, 3, 4, 5]
-// var position;
-// for(position=0; position < list.length; position++)
-// console.log('Position => ' + position + ' Value => '+ list[position])
-
-// break keyword
-var position;
-for(position=0; position < list.length; position++){
-if(position >= 3) break
-console.log('Position => ' + position + ' Value => '+ list[position])
+function mSum()
+{
+    var num1 = 10;
+    var num2 = 20;
+    var sum =num1 + num2;
+    return sum
 }
-
-// continue keyword
-var position
-for(position = 0; position < list.length; position++)
-{if(position % 2 !==0) continue
-    console.log('Position => ' + position + ' Value => '+ list[position])
-}
+console.log(mSum())
