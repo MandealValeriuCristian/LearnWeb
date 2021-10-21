6b085cba5902d114c5a21890aba7f4dc5ca87568
Valeriu-Cristian Mandeal <mandealvcristian@gmail.com>
2021-10-21 12:14:57 +0300

    Select HTML Elements

--- learnJS.js
+++ learnJS.js
@@ -1,13 +1,7 @@
-sum(5, 10)
-sum(30, 22)
-function sum(num1, num2){
-    console.log(num1 + num2)
-}
-var total =20
-var sum2 = function(num1, num2){
-    console.log(total)
-    total = num1 + num2
-    var total
-    console.log(total)
-}
-sum2(90,2)
+console.log('Script Loaded')
+
+console.log(document.getElementById("logo"));
+
+console.log(document.getElementsByClassName('blog-card'))
+
+console.log(document.getElementsByTagName('h3'))
