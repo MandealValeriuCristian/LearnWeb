8c83d16dbc4fd8620159be52ca8e8cf054a28359
Valeriu-Cristian Mandeal <mandealvcristian@gmail.com>
2021-10-21 11:49:08 +0300

    Function Hoisting

--- learnJS.js
+++ learnJS.js
@@ -1,4 +1,13 @@
-console.log(num1);
-num1 = 10;
-console.log(num1);
-var num1
+sum(5, 10)
+sum(30, 22)
+function sum(num1, num2){
+    console.log(num1 + num2)
+}
+var total =20
+var sum2 = function(num1, num2){
+    console.log(total)
+    total = num1 + num2
+    var total
+    console.log(total)
+}
+sum2(90,2)
