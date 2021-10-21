b6595de505ec75845b39aa6c56996ce282e43e02
Valeriu-Cristian Mandeal <mandealvcristian@gmail.com>
2021-10-21 10:01:07 +0300

    Function Arguments

--- learnJS.js
+++ learnJS.js
@@ -1,8 +1,24 @@
-function mSum()
-{
-    var num1 = 10;
-    var num2 = 20;
-    var sum =num1 + num2;
-    return sum
+function greetings(personName){
+    console.log('Welcome '+personName)
 }
-console.log(mSum())
+
+greetings('Qaifi')
+greetings('Nate')
+greetings('Vijay')
+
+function totalSum(num1, num2){
+    console.log('num1 => '+num1)
+    console.log('num2 => '+num2)
+    console.log(num1+num2)
+}
+totalSum(2,7)
+totalSum(22,38,38)
+function mSum(num1=10,num2=30,num3=40){
+    console.log('num1 => '+num1)
+    console.log('num2 => '+num2)
+    console.log('num3 => '+num3)
+    if(num3 !== undefined)
+    console.log(num1+num2+num3)
+    else console.log(num1+num2)
+}
+mSum(1)
