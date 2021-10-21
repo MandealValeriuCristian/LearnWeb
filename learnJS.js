13b210da0188bd33fded7167801bc70ca29dd1b1
Valeriu-Cristian Mandeal <mandealvcristian@gmail.com>
2021-10-21 11:33:00 +0300

    Scope and Environment

--- learnJS.js
+++ learnJS.js
@@ -1,24 +1,8 @@
-function greetings(personName){
-    console.log('Welcome '+personName)
+var num1 = 10;
+console.log(num1)
+function sum(num2)
+{
+    console.log(num1)
+    console.log(num1 + num2)
 }
-
-greetings('Qaifi')
-greetings('Nate')
-greetings('Vijay')
-
-function totalSum(num1, num2){
-    console.log('num1 => '+num1)
-    console.log('num2 => '+num2)
-    console.log(num1+num2)
-}
-totalSum(2,7)
-totalSum(22,38,38)
-function mSum(num1=10,num2=30,num3=40){
-    console.log('num1 => '+num1)
-    console.log('num2 => '+num2)
-    console.log('num3 => '+num3)
-    if(num3 !== undefined)
-    console.log(num1+num2+num3)
-    else console.log(num1+num2)
-}
-mSum(1)
+sum(20);
