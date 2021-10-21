3db5d7525df74afbf5c869a19727293e12ac959a
Valeriu-Cristian Mandeal <mandealvcristian@gmail.com>
2021-10-21 12:20:28 +0300

    Query Selectors

--- learnJS.js
+++ learnJS.js
@@ -1,7 +1,3 @@
 console.log('Script Loaded')
 
-console.log(document.getElementById("logo"));
-
-console.log(document.getElementsByClassName('blog-card'))
-
-console.log(document.getElementsByTagName('h3'))
+console.log(document.querySelectorAll('.blog-card img'))
