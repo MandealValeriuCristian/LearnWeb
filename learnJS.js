9ea146c45fd0a7e697253d9e34d5119ba9b44c49
Valeriu-Cristian Mandeal <mandealvcristian@gmail.com>
2021-10-23 16:31:09 +0300

    Updating Styles

--- learnJS.js
+++ learnJS.js
@@ -1,15 +1,13 @@
 console.log('Script Loaded')
+var countdownElement = document.getElementById("countdown")
+var bgImage = document.getElementById("bg-image")
+var initialCountdownVal = countdownElement.innerHTML;
 
-var countdownElement=document.getElementById('countdown')
-var bgImageElement = document.getElementById('bg-image')
-
-var initialCountdownVal = countdownElement.innerHTML
-
-setInterval(function() {
-    initialCountdownVal = initialCountdownVal > 0 ? initialCountdownVal -1 : 0;
-
-    countdownElement.innerHTML=initialCountdownVal;
-    var backImgPath = initialCountdownVal % 2 === 0? '/assets/background-1.jpg' : '/assets/background-2.jpg'
-    bgImageElement.className = 'BackImage'
-    bgImageElement.src = backImgPath;
+var interval = setInterval(function() {
+    initialCountdownVal = initialCountdownVal > 0 ? initialCountdownVal-1 : 0;
+    countdownElement.innerHTML = initialCountdownVal
+    countdownElement.style.fontSize = initialCountdownVal * 100 + "px"
+    bgImage.style.width= initialCountdownVal*10+"%"
+   if(initialCountdownVal <= 0)
+    clearInterval(interval)
 }, 1000);
