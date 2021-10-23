ab3fb770100aefb08674cf5c0d8b19a240e5026c
Valeriu-Cristian Mandeal <mandealvcristian@gmail.com>
2021-10-23 21:18:54 +0300

    Form Events

--- learnJS.js
+++ learnJS.js
@@ -1,21 +1,19 @@
-console.log("Script Loaded")
-var mainHeading = document.getElementById("main-heading");
-var btnDecreaseFont = document.getElementById("btn-decrease");
-var btnIncreaseFont = document.getElementById("btn-increase");
-
-var initialFontSize = window.getComputedStyle(mainHeading).fontSize;
-initialFontSize = parseInt(initialFontSize.substr(0, (initialFontSize.Length - 2)));
-
-// mainHeading.style.fontSize = parseInt(initialFontSize) +100 +"px"
-btnIncreaseFont.onclick = function(){
-    initialFontSize += 10;
-    mainHeading.style.fontSize = initialFontSize+"px";
-}
-btnDecreaseFont.addEventListener("click", function(){
-    initialFontSize -= 10;
-    mainHeading.style.fontSize = initialFontSize+"px";
+var username = document.getElementById('username');
+var loginForm = document.getElementById('login-form');
+username.addEventListener('input', function(){
+    var currentValue = event.target.value;
+    currentValue = currentValue.toUpperCase();
+    username.value=currentValue;
+    console.log(currentValue)
 })
-// console.log(btnDecreaseFont.style)
-// console.log(btnIncreaseFont.style.marginRight)
+username.addEventListener('focus', function(){
+    console.log('Element Focussed')
+})
+username.addEventListener('blur', function(){
+    console.log('Element Lost Focus')
+})
 
-// console.log(window.getComputedStyle(btnDecreaseFont).textdecoration)
+loginForm.addEventListener('submit', function(e){
+    alert('Submit Button Clicked')
+    e.preventDefault();
+})
