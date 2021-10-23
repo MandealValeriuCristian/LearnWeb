555188ee4f32bd9ed2600cf414f00e346e57f031
Valeriu-Cristian Mandeal <mandealvcristian@gmail.com>
2021-10-23 17:25:48 +0300

    Add and Remove Classes from HTML Elements

--- learnJS.js
+++ learnJS.js
@@ -1,16 +1,20 @@
 console.log('Script Loaded')
-var btn = document.getElementById('btn-click')
-var mainDiv = document.querySelector("header div")
-function onBtnClick(){
-    var randomNum = Math.floor(Math.random() * 255)
-    
-    mainDiv.style.backgroundColor = "rgb("+Math.floor(Math.random() * 255)
-    + "," + Math.floor(Math.random() * 255) + ","
-     + Math.floor(Math.random() * 255)+")"
-}
-// btn.onclick = onBtnClick;
-// btn.addEventListener('click', function(){
-//     alert('Anonymous Function Called')
-// })
+var mainHeading = document.getElementById("main-heading");
 
-btn.addEventListener('click', onBtnClick)
+ var btn = document.getElementById('btn-click')
+
+var sidebar = document.getElementById('sidebar')
+var toggleButton = document.getElementById('toggle-icon')
+ mainHeading.classList.add('big')
+ btn.addEventListener('click', function() {
+     if(sidebar.classList.contains('show')){
+        sidebar.classList.remove('show')
+        sidebar.classList.add('hide')
+     }
+     else{
+        sidebar.classList.add('show')
+        sidebar.classList.remove('hide')
+     }
+    sidebar.classList.remove('show')
+})
+
