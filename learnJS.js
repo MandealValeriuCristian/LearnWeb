faac507cad919a2a2f5218914edf105d63b69e73
Valeriu-Cristian Mandeal <mandealvcristian@gmail.com>
2021-10-24 15:11:50 +0300

    Create HTML Elements - Part 2

--- learnJS.js
+++ learnJS.js
@@ -1,16 +1,36 @@
 var list = document.getElementById('todo-list')
 var btnAdd = document.getElementById('add-item')
-
-
-
-
-btnAdd.addEventListener('click', function(){
-    var newListElement = document.createElement('li')
-    var textNode = document.createTextNode('List Item '+ (list.childElementCount + 1));
-    newListElement.appendChild(textNode)
-    newListElement.id = "item" + (list.childElementCount + 1);
-    // console.log(newListElement)
-    var firstItem = list.firstElementChild
-    // list.appendChild(newListElement, firstItem)
-    list.insertBefore(newListElement, firstItem)
+var inputBox = document.getElementById('todo-input')
+var currentInputValue = '';
+inputBox.addEventListener('input', function(e){
+    currentInputValue = e.target.value;
 })
+
+inputBox.addEventListener('keyup', function(e){
+    if(e.keyCode == 13)
+    {
+        addListItem();
+    }
+    // alert('Enter Clicked')
+})
+function addListItem(){
+    if(currentInputValue !== undefined && currentInputValue !== null 
+        && currentInputValue !== '')
+        {
+            var newListElement = document.createElement('li')
+            var textNode = document.createTextNode(currentInputValue);
+            newListElement.appendChild(textNode)
+            newListElement.id = "item" + (list.childElementCount + 1);
+            
+            
+            list.appendChild(newListElement)
+            console.log(list.childElementCount)
+            inputBox.value = ''
+            currentInputValue = ''
+        
+        }
+        else{
+            alert('Please enter a valid TODO item')
+        }
+    }
+btnAdd.addEventListener('click', addListItem)
